8cf9aa34b28d184a8fb4a2c55956f982e563d6f3
Pedro Antônio <pedro.riber201726@gmail.com>
2026-04-01 18:53:31 -0300

    a

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ function botao() {
 
     let nome = document.getElementById("name").value
     let password = document.getElementById("senha").value
-
     fetch("http://localhost:5000/dados", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            mensagem: nome, 
+            mensagem: nome,
             senha: password
         })
     })
